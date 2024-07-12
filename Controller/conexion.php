24dc1b10bf9b9b53854702b41cf34626921f7231
Josue Loria <jmiranda90400@ufide.ac.cr>
2024-07-12 14:28:49 -0600

    Home de pagina y php
    Agregue la conexion de base de datos ademas de la plantilla para el home de la pagina, sigo cambiando varas

---
+++ Controller/conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$pass = "1234";
+$db = "Proyecto_MapaInteractivo";
+
+// Crear la conexión
+$conn = new mysqli($server, $user, $pass, $db);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa";
+
+?>
